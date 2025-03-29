eb979fc4f4bf28b18aeebf53aadb95bc849667f9
AlexSolcan <solcan.lazar@gmail.com>
2025-03-29 16:42:57 +0200

    Fisrt commit

---
+++ start_interfata_tmos.m
@@ -0,0 +1,16 @@
+clear all;
+close all;
+
+z = 1;
+RS=1;
+VDD = 14;
+BETA = 0.5;
+RG1 = 2;
+RG2 = 4;    
+RD = 5;
+Vp = 2;
+Va = 100;
+f = 100;
+V = 0;
+A = 2;
+interfata_tmos(z,RS,VDD,BETA,RG1,RG2,RD,Vp,Va,f,V,A);
